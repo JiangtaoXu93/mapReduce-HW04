4d796ff1551aeeb93d91467ab80c19516dc65d1a
Joyal Shah <shah.jo@husky.neu.edu>
2017-10-09 20:15:03 -0400

    Commiting FlightCompositeKey Class. Pending CompareTo implementation.

--- src/main/java/org/neu/data/FlightCompositeKey.java
+++ src/main/java/org/neu/data/FlightCompositeKey.java
@@ -3,27 +3,77 @@ package org.neu.data;
 import java.io.DataInput;
 import java.io.DataOutput;
 import java.io.IOException;
-import org.apache.hadoop.io.Text;
-import org.apache.hadoop.io.WritableComparable;
+
+import org.apache.hadoop.io.*;
 
 public class FlightCompositeKey implements WritableComparable<FlightCompositeKey> {
 
+    private Text month;
+    private Text airport;
+    private Text airline;
+    private IntWritable delay;
+    private IntWritable count;
 
-  private Text month;
-  private Text entity;
+    public Text getMonth() {
+        return month;
+    }
 
-  @Override
-  public void write(DataOutput dataOutput) throws IOException {
+    public void setMonth(Text month) {
+        this.month = month;
+    }
 
-  }
+    public Text getAirport() {
+        return airport;
+    }
 
-  @Override
-  public void readFields(DataInput dataInput) throws IOException {
+    public void setAirport(Text airport) {
+        this.airport = airport;
+    }
 
-  }
+    public Text getAirline() {
+        return airline;
+    }
 
-  @Override
-  public int compareTo(FlightCompositeKey o) {
-    return 0;
-  }
+    public void setAirline(Text airline) {
+        this.airline = airline;
+    }
+
+    public IntWritable getDelay() {
+        return delay;
+    }
+
+    public void setDelay(IntWritable delay) {
+        this.delay = delay;
+    }
+
+    public IntWritable getCount() {
+        return count;
+    }
+
+    public void setCount(IntWritable count) {
+        this.count = count;
+    }
+
+    @Override
+    public void write(DataOutput dataOutput) throws IOException {
+        month.write(dataOutput);
+        airport.write(dataOutput);
+        airline.write(dataOutput);
+        delay.write(dataOutput);
+        count.write(dataOutput);
+    }
+
+    @Override
+    public void readFields(DataInput dataInput) throws IOException {
+        month.readFields(dataInput);
+        airport.readFields(dataInput);
+        airline.readFields(dataInput);
+        delay.readFields(dataInput);
+        count.readFields(dataInput);
+    }
+
+    @Override
+    public int compareTo(FlightCompositeKey o) {
+        return 0;
+    }
 }
